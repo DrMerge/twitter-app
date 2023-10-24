610e94ec018b3cc40119b9494ad464c833fbe45a
DrMerge <eccretav@gmail.com>
2023-10-24 13:52:01 +0100

    Atomic

--- controllers/OTPController.js
+++ controllers/OTPController.js
@@ -9,31 +9,30 @@ const client = require("twilio")(
 const otpAUTH = async (req, res) => {
   try {
     const { otp } = req.body;
-    const encrptedPhone_No = req.cookies.Verf_ID;
+    const encrptedID = req.cookies.Verf_ID;
 
-    jwt.verify(
-      encrptedPhone_No,
-      process.env.UTILITY_ENCRYPTION,
-      async (err, phone_No) => {
-        if (err) return res.status(401).send("Unable to validate Phone Number");
+    jwt.verify(encrptedID, process.env.UTILITY_ENCRYPTION, async (err, ID) => {
+      if (err) return res.status(401).send("Unable to validate Identity");
 
-        await client.verify.v2
-          .services(process.env.SERVICEID)
-          .verificationChecks.create({ to: `+234${phone_No}`, code: otp })
-          .then(async (verification_check) => {
-            if (verification_check.status != "approved")
-              return res.status(400).json({ error: "Please Retry OTP" });
+      await client.verify.v2
+        .services(process.env.SERVICEID)
+        .verificationChecks.create({ to: `+234${ID.phone_No}`, code: otp })
+        .then(async (verification_check) => {
+          if (verification_check.status != "approved")
+            return res.status(400).json({ error: "Please Retry OTP" });
 
-            const user = await UsersDB.findOne({ phone_No: phone_No });
-
-            user.phone_NoVerfied = true;
-
-            await user.save();
-            console.log("\n Verification status: " + verification_check.status);
-            res.status(200).json({ url: `http://${url}:4000/auth` });
+          const result = await UsersDB.create({
+            username: ID.username,
+            email: ID.email,
+            phone_No: ID.phone_No,
+            password: ID.hashedPwd,
+            refreshToken: "",
           });
-      }
-    );
+
+          console.log("\n Verification status: " + verification_check.status);
+          res.status(200).json({ url: `http://${url}:4000/auth` });
+        });
+    });
   } catch (err) {
     console.log(err);
   }
