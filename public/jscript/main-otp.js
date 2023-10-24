c1f6eb9141b0ea7d1a356694794ae78435a1f348
DrMerge <eccretav@gmail.com>
2023-10-24 14:36:59 +0100

    worked on timer

--- public/jscript/main-otp.js
+++ public/jscript/main-otp.js
@@ -1,5 +1,6 @@
 const submitBtn = document.getElementById("submitBtn");
 const url1 = "34.205.33.147";
+const msg = document.getElementById("msg");
 
 submitBtn.onclick = async (e) => {
   e.preventDefault();
@@ -18,29 +19,43 @@ submitBtn.onclick = async (e) => {
       return response.json();
     })
     .then((result) => {
-      if (result.message === "User not found")
+      if (result.message === "User not found") {
         return window.location.replace(url);
+      }
+
       if (result.error) {
-        const msg = document.getElementById("msg");
-        msg.textContent = `Didn't get OTP? Please retry in 30 seconds`;
+        let durationInSeconds = 30; // Set the timer duration to 30 seconds
 
-        const resendBtn = document.createElement("button");
-        const body = document.body;
+        function updateTimer() {
+          // Calculate minutes and seconds
+          const minutes = Math.floor(durationInSeconds / 60);
+          const seconds = durationInSeconds % 60;
 
-        resendBtn.style.backgroundColor = "blue";
-        resendBtn.style.color = "white";
+          // Format the time as "mm:ss"
+          const timeString = `${minutes.toString().padStart(2, "0")}:${seconds
+            .toString()
+            .padStart(2, "0")}`;
 
-        resendBtn.textContent = `Resend OTP`;
+          // Update the timer display in the 'msg' element
+          msg.textContent = `Didn't get OTP? Please retry in ${timeString}`;
 
-        resendBtn.addEventListener("click", function () {
-          // Replace 'your-url-here' with the URL you want to navigate to
-          window.location.href = "your-url-here";
-        });
+          // Decrement the time left
+          durationInSeconds--;
 
-        body.appendChild(resendBtn);
-      }else{
-      const redirectUrl = result.url;
-      window.location.replace(redirectUrl);}
+          // If the timer reaches 0, stop the timer and perform an action (e.g., enable a resend button)
+          if (durationInSeconds < 0) {
+            clearInterval(timer);
+            // You can add your action here
+          }
+        }
+
+        // Call the updateTimer function every second
+        updateTimer();
+        const timer = setInterval(updateTimer, 1000);
+      } else {
+        const redirectUrl = result.url;
+        window.location.replace(redirectUrl);
+      }
     })
     .catch((error) => {
       console.error("Error:", error);
